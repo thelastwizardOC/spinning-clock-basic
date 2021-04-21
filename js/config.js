5dd4d007372725955bf89c7b24e282ba2536331c
Tran Duc Tri <tdtri.115@gmail.com>
2021-04-21 11:29:09 +0700

    initial commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+var config = {
+  language_type: 2,
+  font_color: "#808080",
+  pointer_color: "#FF0000",
+
+  audio: 1,
+  audio_name: "Before I Sleep (Dance) - Elena.mp3",
+
+  // background_style: 1,
+  // background_picture: "bg.jpg",
+  // background_video: "3.mp4",
+  // background_color: "#000000",
+};
